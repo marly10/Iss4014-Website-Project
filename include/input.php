e7a362c0e2966746fe71d7dc2c2829b39681ee20
marly10 <mckmarly@icloud.com>
2020-04-22 14:12:11 -0700

    input file works and stores data into database

--- include/input.php
+++ include/input.php
@@ -3,7 +3,7 @@
   Class: ISS4014-1 SP20 - Data Base Systems And Web Integration
   Due Date: Due Apr 27
   ----->
-<!DOCTYPE HTML>
+  <!DOCTYPE HTML>
 <html>
 
 <head>
@@ -15,73 +15,90 @@
 </head>
 
 <body>
-    <label for="fname">First Name</label><br>
+
     <!---------HTML List  Ends here--------->
 
     <!---------Contact Form Begins here--------->
 
-    <form action="input.php" method="POST" enctype="multipart/form-data">
+<form action="input.php" method="POST" enctype="multipart/form-data">
 
-        <label for="fname">Question</label><br>
-        <textarea type="text" name="model" value="" required></<textarea>
+<label for="Question">Question</label><br>
+<input type="text" name="question" required>
 
-<br><label for="fname">Answer1</label><br> 
-<input type="text" name="year" value="" required/>
+<br><label for="Answer1">Answer1</label><br> 
+<input type="text" name="answer1" required/>
 
-<br><label for="fname">Answer2</label><br> 
-<input type="number" name="mileage" value="" required/>
+<br><label for="Answer2">Answer2</label><br> 
+<input type="text" name="answer2" required/>
 
-<br><label for="fname">Answer3</label><br> 
-<input type="number" name="price" value="" required/>
+<br><label for="Answer3">Answer3</label><br> 
+<input type="text" name="answer3" required/>
 
-<br><label for="fname">Answer4</label><br> 
-<input type="text" name="color" value="" required/>
+<br><label for="Answer4">Answer4</label><br> 
+<input type="text" name="answer4"required/>
 
-<br><label for="fname">AnswerToQuestion(Enter numeric value)</label><br> 
-<input type="number" name="body" value="" required/>
+<br><label for="AnswerToQuestion">AnswerToQuestion(Enter numeric value)</label><br> 
+<input type="number" name="numanswer" value="" min="1" max="2"required/>
 
+<br><label for="Count">Count</label><br> 
+<input type="number" name="count" value="0" />
 
-<br><label for="fname">Count</label><br> 
-<input type="hidden" name="trans" value="0" />
-
-<br><label for="fname">Marvel Image</label><br> 
+<br><label for="image">Marvel Image</label><br> 
 <input type="file" name="image" id="image" />
 
 
-<br><input type="submit" name="insert" id="insert" value="Insert" class="btn btn-info" /> </form>
+<br><input type="submit" name="insert" id="insert" value="Insert" class="btn btn-info" /> 
+
+</form>
 
 
-    <?php
+<?php
 
 //if statement that is triggered on the submit button in the form being pressed
- if(isset($_POST["Submit"]))  
- {  
-    //form variables captured using POST & superglobal
-    $fname = $_POST['firstname'];
-    $lname = $_POST['lastname'];
-    $email = $_POST['emailAdd'];
-    $fActivity = $_POST['footballActivity'];
-    $extraActivity = $_POST['subject'];
+//ERROR_REPORTING(E_ALL); used to debug where the error was. if($value >=500 && $value <=600 )
+//{
+ // echo "value is between 500 and 600";
+//}
 
-    //sql query being executed when if statement is executed
-    $query = "INSERT INTO CONTACT_FORM(  F_FNAME, F_LNAME, F_EMAIL, F_ACTIVITY, F_COMM) VALUES ( '$fname', '$lname', '$email', '$fActivity', '$extraActivity')";  
+//database login information        
+$host    = "iss40142020.csr3g0achpwq.us-east-1.rds.amazonaws.com";
+$user    = "marlyMck1101";
+$pass    = "systemFileInt1101";
+$db_name = "test_db"; 
+$connect = mysqli_connect($host, $user, $pass, $db_name);
+
+     if (!$connect) {
+ die ("Connection failed: ". mysqli_connect_errno() . ": ".
+mysqli_connect_error());
+}
+
+else{
+
+echo 'We good';
+}
+ 
+
+ if(isset($_POST["insert"]))  
+ {  
+
+     $question = $_POST['question'];
+     $answer1 = $_POST['answer1'];
+     $answer2 = $_POST['answer2'];
+     $answer3 = $_POST['answer3'];
+     $answer4 = $_POST['answer4'];
+     $numAnswer = $_POST['numanswer'];
+     $count  = $_POST['count'];
+     $img = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
+          
+      $query = "INSERT INTO TRIVIA_TABLE(T_QUESTION, T_ANSWER1, T_ANSWER2, T_ANSWER3, T_ANSWER4, T_ANSWER, T_COUNT, T_IMG) VALUES ('$question', '$answer1', '$answer2', '$answer3', '$answer4', '$numAnswer', '$numAnswer', '$img')";  
      
-        //message posted on alert when message is sent.
-      $message = "Thank you for reaching out: ".$fname."\\nWe will have a great time chatting about: ".$fActivity."";
      
-      //if used to alert on screen the message has been sent properly
       if(mysqli_query($connect, $query))  
       {  
-           echo '<script>alert("'.$message.'")</script>';  
-      }
-      //else for the message error to user.
-      else
-      {
-          echo 'Sorry, your message was not sent. Currently we are working to solve this issue!';
-        
-    }
-
- } //end isset statement
+           echo '<script>alert("Image Inserted into Database")</script>';  
+      }  
+ } 
+ //end isset statement
 ?>
 
     <!---------Contact Form ends here--------->
@@ -91,44 +108,3 @@
 </body>
 
 </html>
-
-
-<br />
-
-
-<?php  
-
-//ERROR_REPORTING(E_ALL); used to debug where the error was.
-
-//database login information        
-$host    = "iss40142020.csr3g0achpwq.us-east-1.rds.amazonaws.com";
-$user    = "marlyMck1101";
-$pass    = "systemFileInt1101";
-$db_name = "fanPage_ch20"; 
-$connect = mysqli_connect($host, $user, $pass, $db_name);
-
-
- if(isset($_POST["insert"]))  
- {  
-
-     $make = $_POST['make'];
-     $model = $_POST['model'];
-     $year = $_POST['year'];
-     $mileage = $_POST['mileage'];
-     $price = $_POST['price'];
-     $color = $_POST['color'];
-     $body = $_POST['body'];
-     $tran = $_POST['trans'];
-     
-      $file = addslashes(file_get_contents($_FILES["image"]["tmp_name"]));  
-          
-      $query = "INSERT INTO tbl_images(make, model, year, mileage, price, color, body, trans, name) VALUES ('$make', '$model', '$year', '$mileage', '$price', '$color', '$body', '$tran', '$file')";  
-     
-     
-      if(mysqli_query($connect, $query))  
-      {  
-           echo '<script>alert("Image Inserted into Database")</script>';  
-      }  
- } 
-
- ?>
