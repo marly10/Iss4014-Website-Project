e6b7da499c6ddee00de05def4c30a9777be9484f
joshb455 <45502270+joshb455@users.noreply.github.com>
2020-04-22 21:03:47 -0700

    A few questions
    5 questions as a good start to test out the trivia game, I will work on getting the longblob images inserted with these questions tomorrow.  I'll add many more questions once I see how these ones work out.

--- db.sql
+++ db.sql
@@ -12,3 +12,9 @@
   T_IMG  LONGBLOB NOT NULL
   );
   
+ /*Adding trivia questions to trivia table, some questions will need images, not sure the syntax for inserting LONGBLOBs yet*/
+INSERT INTO TRIVIA_TABLE(T_ID, T_QUESTION, T_ANSWER1, T_ANSWER2, T_ANSWER3, T_ANSWER4, T_ANSWER) VALUES(1, "What is the name of this character?", "Loki", "Captain Marvel", "Thor", "Hawkeye", "Thor");
+INSERT INTO TRIVIA_TABLE(T_ID, T_QUESTION, T_ANSWER1, T_ANSWER2, T_ANSWER3, T_ANSWER4, T_ANSWER) VALUES(2, "Which film was the first of the Marvel Cinematic Universe?", "Avengers: Age of Ultron", "Iron Man", "Black Panther", "The Guardians of the Galaxy", "Iron Man");
+INSERT INTO TRIVIA_TABLE(T_ID, T_QUESTION, T_ANSWER1, T_ANSWER2, T_ANSWER3, T_ANSWER4, T_ANSWER) VALUES(3, "What is the name of Tony Stark's significant other?", "Pepper Potts", "Sunset Baine", "Carol Danvers", "Black Widow", "Pepper Potts");
+INSERT INTO TRIVIA_TABLE(T_ID, T_QUESTION, T_ANSWER1, T_ANSWER2, T_ANSWER3, T_ANSWER4, T_ANSWER) VALUES(4, "What is Thanos trying to obtain in the Avengers: Endgame movie?", "Titanium", "Black Panther's soul", "The Infinity Stones", "Thor's Hammer", "The Infinity Stones");
+INSERT INTO TRIVIA_TABLE(T_ID, T_QUESTION, T_ANSWER1, T_ANSWER2, T_ANSWER3, T_ANSWER4, T_ANSWER) VALUES(5, "Where is Black Panther the king of?", "New York", "Wakanda", "Asgard", "Muir Island", "Wakanda");
